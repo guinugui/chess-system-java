904404ae24a84dc50f40e9fce1a508dc23baf21e
Guilherme Ribeiro <guilhermevendaseprodutos@gmail.com>
2024-02-05 18:05:38 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
@@ -12,13 +14,19 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		ChessMatch chessMatch = new ChessMatch ();
-		UI.printBoard(chessMatch.getPieces());
 		
-		
-		
-		
-		
-		sc.close();
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece caputuredPiece = chessMatch.performChessMove(source, target);
+		}
 
 	}
 
